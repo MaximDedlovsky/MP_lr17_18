aa09928240f9dd86287771edb0df5c22173dbfbf
MaximDedlovsky <maxim.dedlovsky@yandex.ru>
2016-12-17 12:28:24 +0500

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,44 @@
+def sumMatrix(matrix1: list, matrix2: list) -> list:
+    result = []
+    if len(matrix1) != len(matrix2) or len(matrix1[0]) != len(matrix2[0]):
+        return False
+    for i in range(len(matrix1)):
+        row = []
+        for j in range(len(matrix1[i])):
+            row.append(matrix1[i][j], matrix2[i][j])
+        result.append(row)
+    return result
+
+
+def mulMatrix(matrix1: list, matrix2: list) -> list:
+    if len(matrix1[0]) != len(matrix2):
+        return False
+    sum = 0
+    rowList = []
+    result = []
+    for i in range(len(matrix1)):
+        for j in range(len(matrix2[0])):
+            for k in range(len(matrix1[0])):
+                sum += matrix1[i][k] * matrix2[k][j]
+            rowList.append(sum)
+            sum = 0
+        result.append(rowList)
+        rowList = []
+    return result
+
+
+def GetMatrix(rows: int, cols: int) -> list:
+    matrix = []
+    for i in range(rows):
+        rowList = []
+        for j in range(cols):
+            rowList.append(random.randint(0, 9))
+        matrix.append(rowList)
+    return matrix
+
+
+def PrintMatrix(matrix: list):
+    for row in matrix:
+        for item in row:
+            print(item, end=' ')
+        print()
