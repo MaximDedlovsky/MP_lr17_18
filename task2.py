1eb13367f65765f96ca5efda840762f5970e836f
MaximDedlovsky <maxim.dedlovsky@yandex.ru>
2016-12-17 11:18:48 +0500

    Добавлен ввод для второго задания

--- task2.py
+++ task2.py
@@ -2,3 +2,6 @@ def getFibonacciNum(n: int) -> int:
     if n < 3:
         return 1
     return getFibonacciNum(n - 1) + getFibonacciNum(n - 2)
+
+n = int(input('Enter number: '))
+print('n-th Fibonacci number: {0}'.format(getFibonacciNum(n)))
