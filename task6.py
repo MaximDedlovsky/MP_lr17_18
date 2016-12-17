56726c791ba3cc6232571edb5edca94b75c0f3cd
MaximDedlovsky <maxim.dedlovsky@yandex.ru>
2016-12-17 13:17:41 +0500

    Create task6.py

---
+++ task6.py
@@ -0,0 +1,13 @@
+import re
+
+str = input('Enter string: ')
+words = re.sub('\W', ' ', str).split()
+for i in range(len(words)):
+    words[i] = words[i].lower()
+dictionary = {}
+for word in words:
+    if word in dictionary:
+        dictionary[word] += 1
+    else:
+        dictionary[word] = 1
+print(dictionary)
